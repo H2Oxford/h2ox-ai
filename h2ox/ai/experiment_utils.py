e358a168ffc9f42d2f42bfdb303f1d0e1c4b3643
Tommy Lees <thomas.lees112@gmail.com>
2022-02-01 13:09:48 +0000

    update toi plot losses

--- h2ox/ai/experiment_utils.py
+++ h2ox/ai/experiment_utils.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from datetime import datetime
 import yaml
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def create_model_experiment_folder(
     data_dir: Path, experiment_name: str, add_datetime: bool = False
@@ -19,3 +20,15 @@ def create_model_experiment_folder(
 def dump_config(config: dict, config_path: Path) -> None:
     with open(config_path, "w") as f:
         yaml.dump(config, f, default_flow_style=False)
+
+
+def plot_losses(filepath: Path, losses: np.ndarray, val_losses: np.ndarray):
+    f, ax = plt.subplots(figsize=(12, 4))
+    ax.plot(losses, label="Training")
+    ax.plot(val_losses, label="Validation")
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.set_xlabel("Epoch")
+    ax.set_xlabel("Loss")
+    ax.legend()
+    f.savefig(filepath / "losses.png")
