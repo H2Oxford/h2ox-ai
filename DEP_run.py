bd3e6d5a9177abd31d3099989ebf8c8c19694fbe
Tommy Lees <thomas.lees112@gmail.com>
2022-02-01 11:22:43 +0000

    normalize data

--- DEP_run.py
+++ DEP_run.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from types import MappingProxyType
 from typing import List, Dict, Any, Callable
 
 import torch
@@ -10,14 +9,18 @@ from h2ox.ai.dataset import FcastDataset
 from h2ox.ai.model import initialise_model
 from h2ox.ai.train import initialise_training, train, train_validation_split
 from h2ox.ai.scripts.utils import load_zscore_data, load_samantha_updated_data
+from h2ox.ai.data_utils import normalize_data
 
 # instantiate the Experiment class
 ex = Experiment("fcast", interactive=True)
 
-
 # TODO: normalize/unnormalize data
 # TODO: save experiments in reproducible way
-# TODO: test dataloader
+# TODO: assert that experiment settings make sense (i.e. variables can be found etc.) def pre_experiment_checks
+# TODO: ensuring config file sets up your data
+# TODO: specify the reservoirs (wrap in a loop for all reservoirs)
+# TODO: uncertainty [stretch goal]
+# TODO: build test dataloaders etc.
 
 
 def initialise_experiment():
@@ -49,23 +52,33 @@ def main(
     # load data
     data_dir = Path.cwd() / "data"
     target, history, forecast = load_zscore_data(data_dir)
-    sam_data = load_samantha_updated_data(data_dir)
-    history = history.merge(target)
+    # sam_data = load_samantha_updated_data(data_dir)
+    # target = sam_data[[target_var]]
+    # history = sam_data
+    # forecast = None
 
     # select site
-    site_target = target.sel(location=[site])
-    site_history = history.sel(location=[site])
-    site_forecast = forecast.sel(location=[site])
+    # site_target = target.sel(location=[site])
+    # site_history = history.sel(location=[site])
+    # site_forecast = forecast.sel(location=[site]) if forecast is not None else None
 
     # train-test split
-    train_forecast = site_forecast.sel(
-        initialisation_time=slice(train_start_date, train_end_date)
+    # normalize data
+    train_history, (history_mn, history_std) = normalize_data(
+        history.sel(time=slice(train_start_date, train_end_date)), static_or_global=True
+    )
+    train_target, (target_mn, target_std) = normalize_data(
+        target.sel(time=slice(train_start_date, train_end_date)), static_or_global=True
+    )
+    train_forecast, (forecast_mn, forecast_std) = normalize_data(
+        forecast.sel(initialisation_time=slice(train_start_date, train_end_date)),
+        time_dim="initialisation_time", static_or_global=True
     )
 
     dd = FcastDataset(
         target=site_target,  # target,
-        history=site_history,  # history,
-        forecast=train_forecast,  # forecast,
+        history=train_history,  # history,
+        forecast=site_forecast,  # forecast,
         encode_doy=encode_doy,
         historical_seq_len=seq_len,
         future_horizon=future_horizon,
@@ -107,7 +120,12 @@ def main(
         epochs=n_epochs,
         val_dl=val_dl,
     )
-    # plt.plot(losses)
+    
+    # f, ax = plt.subplots(figsize=(12, 4))
+    # ax.plot(losses)
+
+
+    # # test
 
 
 def get_correct_keys(conf: Dict[str, Any], func: Callable) -> Dict[str, Any]:
