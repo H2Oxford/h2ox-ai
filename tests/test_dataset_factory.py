adfd6f575858c9609e2b9e8e1b701a0b870d8dec
Tommy Lees <thomas.lees112@gmail.com>
2022-02-08 11:52:20 +0000

    update tests

--- tests/test_dataset_factory.py
+++ tests/test_dataset_factory.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 from h2ox.ai.dataset.dataset_factory import DatasetFactory
 from ruamel.yaml import YAML
 from pathlib import Path
@@ -14,8 +15,31 @@ def get_single_dataunit_dict(cfg, key_to_keep: str):
     return cfg_cp
 
 
+def test_individual_build_data_units(cfg: Dict[str, Any]):
+    # subset of data
+    doy_cfg = get_single_dataunit_dict(cfg, "doy") 
+    forecast_cfg = get_single_dataunit_dict(cfg, "forecast")
+    history_cfg = get_single_dataunit_dict(cfg, "history")
+    targets_cfg = get_single_dataunit_dict(cfg, "targets")
+    
+    # check the dataset construction for each data unit
+    for cfg_ in [targets_cfg, doy_cfg, forecast_cfg, history_cfg]:
+        cfg_type = [str(k) for k in cfg_["data_parameters"]["data_units"].keys()][0]
+        dsf = DatasetFactory(cfg_)
+        arrays = dsf._build_data()
+        
+        if cfg_type == "forecast":
+            assert False
+
+
+def test_creation_of_all_test_data(cfg: Dict[str, Any]):
+    dsf = DatasetFactory(cfg)
+    fcast_ds = dsf.build_dataset()
+    assert False
+
+
 if __name__ == "__main__":
-    yml_path = Path("conf.yaml")
+    yml_path = Path("tests/test_conf.yaml")
     with yml_path.open('r') as fp:
         yaml = YAML(typ="safe")
         cfg = yaml.load(fp)
@@ -24,20 +48,5 @@ if __name__ == "__main__":
     cfg["data_parameters"]["cache_path"] = Path(".").absolute() / "data/cache.nc"
     cfg["data_parameters"]["data_units"]["forecast"]["gdf_path"] = Path(".").absolute() / "data/basins.geojson"
     cfg["data_parameters"]["data_units"]["historic"]["gdf_path"] = Path(".").absolute() / "data/basins.geojson"
-
-    # subset of data
-    doy_cfg = get_single_dataunit_dict(cfg, "doy") 
-    forecast_cfg = get_single_dataunit_dict(cfg, "forecast")
-    history_cfg = get_single_dataunit_dict(cfg, "history")
-    targets_cfg = get_single_dataunit_dict(cfg, "targets")
-    
-    # check the dataset construction for each data unit
-    # [targets_cfg, doy_cfg, forecast_cfg, history_cfg]
-    for cfg_ in [doy_cfg]:
-        cfg_type = [str(k) for k in cfg_["data_parameters"]["data_units"].keys()][0]
-        dsf = DatasetFactory(cfg_)
-        arrays = dsf._build_data()
         
-        if cfg_type == "doy":
-            assert False
-        
+    test_creation_of_all_test_data(cfg)
