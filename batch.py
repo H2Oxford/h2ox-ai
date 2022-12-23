4eb8c5b60c077dfc0e3c54f37100b53ab6f5db85
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-12-23 03:33:32 +0000

    add batch script

---
+++ batch.py
@@ -0,0 +1,43 @@
+import json
+import os
+from datetime import datetime
+
+from h2ox.ai.inference import local_inference
+from h2ox.ai.slackbot import SlackMessenger
+
+
+def main():
+
+    today = datetime.now()
+
+    token = os.environ.get("SLACKBOT_TOKEN")
+    target = os.environ.get("SLACKBOT_TARGET")
+
+    if token is not None and target is not None:
+
+        slackmessenger = SlackMessenger(
+            token=token,
+            target=target,
+            name="H2OX-PIPELINE :: ",
+        )
+    else:
+        slackmessenger = None
+
+    w2w_inference(today, slackmessenger)
+
+    return 1
+
+
+def w2w_inference(today: datetime, slackmessenger: SlackMessenger):
+
+    # step 2-> rerun inference and post results
+    basin_networks = json.loads(os.environ.get("BASIN_NETWORKS"))
+    msg = local_inference(today, basin_networks)
+    if slackmessenger is not None:
+        slackmessenger.message(f"W2W ::: inference: {json.dumps(msg)}")
+
+    return f"w2w inference {today.isoformat()}", 200
+
+
+if __name__ == "__main__":
+    main()
